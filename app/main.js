38d5f84917e9f2a0bbc2107c94794bd8cce46bfb
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-01-21 16:28:53 +0100

    [any message]

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2); // Skip the first two arguments (node path a
 if (args.length < 2) {
   console.error("Usage: ./your_program.sh tokenize <filename>");
   process.exit(1);
-}
+} 
 
 const command = args[0];
 
@@ -14,13 +14,31 @@ if (command !== "tokenize") {
   process.exit(1);
 }
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.error("Logs from your program will appear here!");
 
 const filename = args[1];
-
-
 const fileContent = fs.readFileSync(filename, "utf8");
+
+let lines = fileContent.split("\n");
+for (let i = 0; i < lines.length; i++) {
+  for (let j = 0; j < lines[i].length; j++) {
+    switch (lines[i][j]) {
+      case '(':
+        console.log("LEFT_PAREN");
+        break;
+      case ')':
+        console.log("RIGHT_PAREN");
+    }
+}
+
+
+
+
+
+
+
+
+
 
 if (fileContent.length !== 0) {
   throw new Error("Scanner not implemented");

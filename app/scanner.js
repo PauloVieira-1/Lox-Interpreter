e0ea3c2014d5c85facb28b67acdeea4fa5bb7412
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-02-06 09:12:37 +0100

    [any message]

--- app/scanner.js
+++ app/scanner.js
@@ -26,7 +26,7 @@ class Token {
 	}
 
 	toString() {
-		return `${this.type} ${this.lexeme} ${this.literal}`.replace("  ", "");
+		return `${this.type} ${this.lexeme} ${this.literal}`.replace("  ", " ");
 	}
 }
 

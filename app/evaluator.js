85520e4484ad68160e3b55182431ea82d6b689bb
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-02-08 19:49:53 +0100

    [any message]

--- app/evaluator.js
+++ app/evaluator.js
@@ -91,7 +91,7 @@ class Visitor {
 			case "!=":
 				return left !== right;
 			case "==":
-				return left == right;
+				return left == right || leftEval == rightEval;
 			case "+":
 					if (isFloat(leftEval) && isFloat(rightEval)) {
 						return left + right;
